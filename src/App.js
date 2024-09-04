accc49a9738df4679cbdd3b5f3966740988a2a75
Alberto Rosell Uyarra <roselluyarra@gmail.com>
2024-09-04 12:11:20 +0900

    Set up ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Pomodoro Timer 25 + 5</h1>
+        <div className="length">
+          <div className="labels" id="break-label">
+            <h3>Break length</h3>
+            <div className="arrows-time">
+              <button id="break-decrement">-</button>
+              <span id="break-length">5</span>
+              <button id="break-increment">+</button>
+            </div>
+          </div>
+          <div className="labels" id="session-label">
+            <h3>Session length</h3>
+            <div className="arrows-time">
+              <button id="break-decrement">-</button>
+              <span id="break-length">25</span>
+              <button id="break-increment">+</button>
+            </div>
+          </div>
+        </div>
+        <div id="timer-label">
+          <h2>Session</h2>
+          <h1 id="time-remaining">25:00</h1>
+        </div>
+      </div>
     </div>
   );
 }
